bfb784c7ad7974f5e8c30fa416a0f58def6ad2c5
SergeWilfried <willytheigeek@gmail.com>
2024-10-05 12:15:55 +0000

    fix: ts config

---
+++ packages/server/routers/example.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const exampleRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    // Explicitly type the return value
+    return ctx.prisma.example.findMany() as Promise<Array<{ id: string; createdAt: Date; updatedAt: Date }>>;
+  }),
+});
